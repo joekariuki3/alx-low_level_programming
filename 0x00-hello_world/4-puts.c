4dc9b93180c02856196c5fb489c3ca3a21fefd8d
joekariuki3 <joekariuki3@gmail.com>
2023-02-16 16:01:12 +0300

    Print a sting using put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,13 @@
+/*
+ * include i/o header file
+*/
+
 #include<stdio.h>
+
+/**
+ * main - Print a string
+ * Return: 0
+*/
 int main(void)
 {
 	char my_sentence[] = "\"Programming is like building a multilingual puzzle";
