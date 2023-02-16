0d4e678a07afddc6935ec8e57cf1f84315cdfb02
joekariuki3 <joekariuki3@gmail.com>
2023-02-16 15:17:08 +0300

    Print a \"string using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main(void)
 {
-	char my_sentence[] = '"Programming is like building a multilingual puzzle';
+	char my_sentence[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(my_sentence);
 	
