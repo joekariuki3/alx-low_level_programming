1d8e75ce28065622c6612d610b3ce14849e3462a
joekariuki3 <joekariuki3@gmail.com>
2023-02-16 16:22:33 +0300

    Print a stiing using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+/*
+ * include the i/o file header
+ */
+
+#include<stdio.h>
+
+/**
+ * main -  start function
+ * prints a string using printf
+ * Return: 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
