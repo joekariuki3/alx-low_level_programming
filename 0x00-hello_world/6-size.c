10da218b33fd12a32c2851c5b0e0257b7a8de86a
joekariuki3 <joekariuki3@gmail.com>
2023-02-16 16:42:36 +0300

    Print size of types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+/*
+ * include the i/o file header
+ */
+
+#include<stdio.h>
+
+/**
+ * main -  start function
+ * prints a sizeof
+ * Return: 0
+ */
+int main(void)
+{
+printf("size of char:  %d byte(s)\n", sizeof(char));
+printf("size of int:  %d byte(s)\n", sizeof(int));
+printf("size of long int:  %d byte(s)\n", sizeof(long int));
+printf("size of long long int:  %d byte(s)\n", sizeof(long long int));
+printf("size of float:  %d byte(s)\n", sizeof(float));
+	return (0);
+}
