ae118423d08fecdfc23dd3379109e61079062d9a
joekariuki3 <joekariuki3@gmail.com>
2023-02-16 19:15:29 +0300

    101-quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - start point
+ * Return: 1 sucess
+ */
+int main(void)
+{
+	write(STDOUT_FILENO, "and that piece of art is useful
+			\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
