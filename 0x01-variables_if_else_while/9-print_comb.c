daad35da3bd9490bcae33007100362aa482526d0
joekariuki3 <joekariuki3@gmail.com>
2023-02-17 16:30:56 +0300

    Possible combination of single digit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,16 +10,17 @@
 */
 int main(void)
 {
-int i;
-for (i = 0; i < 10; i++)
-{
-       	putchar(i + '0');
-	if (i != 9)
+	int i;
+
+	for (i = 0; i < 10; i++)
 	{
-	       	putchar(',');
-		putchar(' ');
+		putchar(i + '0');
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
-}
 putchar('\n');
 return (0);
 }
