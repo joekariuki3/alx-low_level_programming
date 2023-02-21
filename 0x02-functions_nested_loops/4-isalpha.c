8d98c4b138861ac668451155b3bb1849e17fb005
joekariuki3 <joekariuki3@gmail.com>
2023-02-21 18:50:11 +0300

    check if a latter is alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include"main.h"
+
+/**
+ * _isalpha - is alphabet?
+ *
+ *@c: is requred as input
+ *
+ * Return: 1 sucess
+ */
+int _isalpha(int c)
+{
+	char start = 'a', upper = 'A';
+
+	while (start <= 'z' && upper <= 'Z')
+	{
+		if (c == start || c == upper)
+		{
+			return (1);
+		}
+		start++;
+	}
+	return (0);
+}
