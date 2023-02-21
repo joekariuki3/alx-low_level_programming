24be91c99958e5d85399fa7d3bcedba69229fbc0
joekariuki3 <joekariuki3@gmail.com>
2023-02-21 23:08:15 +0300

    104 fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,20 +7,17 @@
   */
 int main(void)
 {
-int i, n = 98;
-    unsigned int fib1 = 1, fib2 = 2, sum;
+	int i, n = 98;
+	unsigned int fib1 = 1, fib2 = 2, sum;
 
-    printf("%u, %u", fib1, fib2);
-
-    for (i = 2; i < n; i++)
-    {
-        sum = fib1 + fib2;
-        printf(", %u", sum);
-        fib1 = fib2;
-        fib2 = sum;
-    }
-
-    printf("\n");
-
-    return 0;
+	printf("%u, %u", fib1, fib2);
+	for (i = 2; i < n; i++)
+	{
+		sum = fib1 + fib2;
+		printf(", %u", sum);
+		fib1 = fib2;
+		fib2 = sum;
+	}
+	printf("\n");
+	return (0);
 }
