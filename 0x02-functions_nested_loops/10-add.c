911503f895a895f3c7b08e7dd3ca8837018e51cb
joekariuki3 <joekariuki3@gmail.com>
2023-02-21 22:03:24 +0300

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  * add - Adds two integers
+  *
+  *@int: requred
+  *
+  * Return: result
+  */
+void add(int, int)
+{
+	int a, b, answer = 0;
+
+	answer = a + b;
+	return (answer);
+}
