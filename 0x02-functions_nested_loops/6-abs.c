7a55046098189ee41c16d635c931cae14baed862
joekariuki3 <joekariuki3@gmail.com>
2023-02-21 19:41:24 +0300

    Gets absolute value of a number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include"main.h"
+
+/**
+ * _abs - print absolute value of a number
+ *
+ *@int: is requred as input
+ *
+ * Return: 0 sucess
+ */
+int _abs(int);
+{
+	int i
+	if (i < 0)
+	{
+		i = -i;
+		_putchar(i);
+	}
+	else
+	{
+		_putchar(i);
+	}
+	return (0);
+}
