6a5b8d4311dc3e0257ef04b4829baed0f5a1f643
joekariuki3 <joekariuki3@gmail.com>
2023-02-21 15:38:58 +0300

    Print alphabet with a fuction

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,29 @@
+/*
+ * include header files
+ */
+#include"main.h"
+
+/**
+ * print_alphabet start of function
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char start = 'a';
+
+	while (start <= 'z')
+	{
+		putchar(start);
+		start++;
+	}
+	putchar('\n');
+}
+/**
+ * main - start function
+ * Return: 0 sucess
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
