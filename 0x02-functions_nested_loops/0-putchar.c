4d5c89110d3b957c62792fd9e3c4e9514936607e
joekariuki3 <joekariuki3@gmail.com>
2023-02-21 15:04:20 +0300

    Printing with putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+/*
+ * include header files
+ */
+#include<stdio.h>
+
+/**
+ * main - start program
+ * Return: 0 sucess
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
