be52f233bb772062b68bbab06c796f6f0aac55e0
joekariuki3 <joekariuki3@gmail.com>
2023-02-22 15:08:39 +0300

    fix an infinite loop

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,14 +1,19 @@
-/*
- * include our own header file
- */
-#include"main.h"
+#include <stdio.h>
 
 /**
- * main - test for positive or negative
- *
- * Return: 0 for succes
- */
+* main - causes an infinite loop
+* Return: 0
+*/
 int main(void)
 {
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+	/*i = 0;*/
+	while (i < 10)
+	{
+		putchar(i);
+	}
+	printf("Infinite loop avoided! \\o/\n");
 	return (0);
 }
