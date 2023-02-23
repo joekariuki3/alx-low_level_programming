0547abb9c137262f35c653ba51c33a9385c8d8af
joekariuki3 <joekariuki3@gmail.com>
2023-02-23 10:54:21 +0300

    Print num 0 9 usint _putchar

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include"main.h"
+
+/**
+ *print_numbers - prints numbers from 0 to 9 usint _putchar
+ *
+ * Return: Nothing
+ */
+void print_numbers(void)
+{
+	int start = 0;
+
+	while (start <= 10)
+	{
+		_putchar(start);
+	}
+	_putchar('\n');
+	return (0);
+}
