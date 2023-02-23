349880167421e2dba92d7f2355cd3957ebacc82a
joekariuki3 <joekariuki3@gmail.com>
2023-02-23 19:37:42 +0300

    Print square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include"main.h"
+
+/**
+ * print_square - prints square size of n(size)
+ * @size: int required
+ * Return: 0
+ */
+void print_square(int size)
+{
+	int l;
+
+	l = 0;
+	while (l < size)
+	{
+		int num = 0;
+
+		while (num < size)
+		{
+			_putchar('#');
+			num++;
+		}
+		l++;
+		_putchar('\n');
+	}
+}
