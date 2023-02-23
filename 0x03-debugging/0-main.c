d4bae28df75e5dde9200d371aa13ea9efa083b03
joekariuki3 <joekariuki3@gmail.com>
2023-02-23 09:27:59 +0300

    Test function() to Check for +ve or -ve

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,17 +1,15 @@
-/*
- * include our own header file
- */
-#include"main.h"
+#include "main.h"
 
 /**
- *main - test for positive or negative
- *
- * Return: 0 for succes
- */
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
 int main(void)
 {
-	int i = 0;
+	int i;
 
+	i = 0;
 	positive_or_negative(i);
 
 	return (0);
