fb914c81ff685ea508a5e9036edc2f2b7fb10d3d
joekariuki3 <joekariuki3@gmail.com>
2023-02-27 20:01:51 +0300

    Print string in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -15,14 +15,14 @@ void print_rev(char *s)
 	int i;
 
 	i = 0;
-	while (str[i])
+	while (s[i])
 	{
-		_putchar(str[i]);
+		_putchar(s[i]);
 		i++;
 	}
 	while (i--)
 	{
-		_putchar(str[i]);
+		_putchar(s[i]);
 	}
 	_putchar('\n');
 }
