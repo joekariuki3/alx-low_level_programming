6a255172985c9d4a5085331ece9751d15c0d97ec
joekariuki3 <joekariuki3@gmail.com>
2023-02-27 21:26:37 +0300

    print every other string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -15,11 +15,12 @@ void puts2(char *str)
 	int i;
 
 	i = 0;
-	while (str[i])
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		_putchar(str[i]);
-		i = i + 2;
-
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
 	}
 	_putchar('\n');
 }
