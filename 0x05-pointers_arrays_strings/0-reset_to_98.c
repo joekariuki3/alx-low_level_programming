c82bec27699a1828f76d12dbd583d5bfcd6055f6
joekariuki3 <joekariuki3@gmail.com>
2023-02-27 14:33:35 +0300

    reset a value using pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+/*
+ * include our own header file
+ */
+#include"main.h"
+
+/**
+ * reset_to_98 - updates value that a pointer poits to a n value
+ *
+ * @n: requires a pointer integer variable
+ *
+ * Returns: nothing(void)
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
