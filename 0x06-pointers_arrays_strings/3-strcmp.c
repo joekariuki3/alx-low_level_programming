821d445d00619648e8774fddd40329d37387cfbf
joekariuki3 <joekariuki3@gmail.com>
2023-03-02 15:46:20 +0300

    string compare

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,24 +12,21 @@
 */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
-	int answer;
-
-	while (s1[i] != '\0' || s2[i] != '\0')
+	while (*s1 != '\0' && *s2 != '\0')
 	{
-		if (s1[i] != s2[i])
+		if (*s1 != *s2)
 		{
-			int alp1 = s1[i];
-			int alp2 = s2[i];
-
-			answer = alp1 - alp2;
-			break;
+			return ((*s1 < *s2) ? -1 : 1);
 		}
-		else
-		{
-			return (0);
-		}
-		i++;
+		s1++;
+		s2++;
 	}
-	return (answer);
+	if (*s1 == '\0' && *s2 == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		return ((*s1 < *s2) ? -1 : 1);
+	}
 }
