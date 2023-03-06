6e2b12f3b2f24179891d2a52551657125a6a48dc
joekariuki3 <joekariuki3@gmail.com>
2023-03-06 15:09:17 +0300

    search a sring for a set of char

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strpbrk - Search a string for any of a set of bytes.
+ * @s: string
+ * @accept: string to match
+ * Return: Pointer to the byte in string *s that matches 
+ * one of the bytes in array of char in *accept
+ * or NULL if no such byte is found.
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+	char *k;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (accept[j] != '\0')
+		{
+			if (accept[j] == s[i])
+			{
+				k = &s[i];
+				return (k);
+			}
+			j++;
+		}
+		i++;
+	}
+
+	return (0);
+}
