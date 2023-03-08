767eff102650aca1f0084e1f7a857ca3af600acc
joekariuki3 <joekariuki3@gmail.com>
2023-03-08 12:45:50 +0300

    length of a string

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -12,13 +12,9 @@
  */
 int _strlen_recursion(char *s)
 {
-	int i;
-
 	if (*s != '\0')
 	{
-		_strlen_recursion(s + 1);
-		i = i++;
-		return (i);
+		return( 1 + _strlen_recursion(s + 1));
 	}
 	else
 		return (0);
