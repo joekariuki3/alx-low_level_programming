a7f56662a8b6e675937f9cf06c42bcbdd413563a
joekariuki3 <joekariuki3@gmail.com>
2023-03-13 15:14:40 +0300

    strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -18,7 +18,7 @@ char *_strdup(char *str)
 	{
 		unsigned int i, size;
 
-		for (size = 0; str != '\0'; size++)
+		for (size = 0; str[size] != '\0'; size++)
 
 		name = malloc(size * sizeof(char));
 		if (name == NULL)
