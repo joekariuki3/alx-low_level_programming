478c1d044499bd671ac0cc5ac439f8870394c8af
joekariuki3 <joekariuki3@gmail.com>
2023-03-13 16:18:25 +0300

    concatinate

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,43 @@
+/*
+ * include standard lib
+ * and custom header
+ */
+#include<stdlib.h>
+#include"main.h"
+
+/**
+ *str_concat - takes two strings and put them together
+ *@s1: first string
+ *@s2: second string
+ *Return: char poiter to the new string
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *fstr;
+	int size1, size2, size3, i, j;
+
+	for (size1 = 0; s1[size1] != '\0'; size1++)
+		;
+	for (size2 = 0; s2[size2] != '\0'; size2++)
+		;
+	size3 = size1 + size2;
+	if (fstr == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		fstr = malloc(size3 * sizeof(char) + 1);
+		for (i = 0; i < size1; i++)
+		{
+			fstr[i] = s1[i];
+		}
+		i++;
+		for (j = 0; j < size3; j++)
+		{
+			fstr[i + j] = s2[j];
+		}
+		fstr[j + 1] = '\0';
+		return (fstr);
+	}
+}
