b114a03b1de01bda3a9f1b64170da1cf36b62d12
joekariuki3 <joekariuki3@gmail.com>
2023-03-16 20:27:11 +0300

    reallocating memory of an array

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -3,6 +3,7 @@
  * and main header
  */
 #include<stdlib.h>
+#include<string.h>
 #include"main.h"
 
 /**
@@ -14,18 +15,21 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	int *newptr, *oldptr;
-	unsigned int i;
+	int *newptr;
 
-	oldptr = ptr;
+	if (new_size == 0)
+        {
+                free(ptr);
+                return (NULL);
+        }
+	if (ptr == NULL)
+	{
+		newptr = malloc(new_size);
+		return (newptr);
+	}
 	if (new_size == old_size)
 	{
 		return (ptr);
-	}
-	if (new_size == 0 && ptr != NULL)
-	{
-		free(ptr);
-		return (NULL);
 	}
 	newptr = malloc(new_size);
 	if (newptr == NULL)
@@ -35,10 +39,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	else
 	{
 		/* copy items from old ptr to newptr */
-		for (i = 0; i < old_size; i++)
-		{
-			newptr[i] = oldptr[i];
-		}
+		memcpy(newptr, ptr, (old_size < new_size) ? old_size : new_size);
 		free(ptr);
 		return (newptr);
 	}
