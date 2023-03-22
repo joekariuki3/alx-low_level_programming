10a187e9a80047b97410993d80a21741d0e5597b
joekariuki3 <joekariuki3@gmail.com>
2023-03-22 10:14:26 +0300

    execute funtions stored in an array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include<stddef.h>
+#include"function_pointers.h"
+/**
+ * array_iterator - iterates in an array of function and executes a function
+ * @array: array containing the functions
+ * @size: length of the array containig the functions
+ * @action: pointer to the fuction being executed
+ * Return: nothing
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned long int i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
