f0bd394411401570bc1b31e692c9e40c5e112e5c
joekariuki3 <joekariuki3@gmail.com>
2023-03-22 10:56:17 +0300

    use function poiter to compare integer in an array of numbers

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,31 @@
+#include<stdlib.h>
+#include"function_pointers.h"
+
+/**
+ * int_index - function to search for integer
+ * @array: array containing the elements
+ * @size: number of elements in the array
+ * @cmp: pointer to the function that compares the integers in the array
+ * Return: integer
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+
+	int i;
+
+	if (size <= 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+			{
+				return (i);
+			}
+		}
+	}
+	return (-1);
+}
