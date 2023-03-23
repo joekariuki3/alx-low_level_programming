a1bdfcbd1c1dca3c08bd80174620597b494a8d02
joekariuki3 <joekariuki3@gmail.com>
2023-03-23 12:56:50 +0300

    sum arguments using va_arguments

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,28 @@
+#include"variadic_functions.h"
+
+/**
+ * sum_them_all - sums all the arguments
+ * @n: number of arguments passed 
+ * Return sum for sucess and 0 if arguments == 0
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	if (n == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		unsigned int i, sum;
+		va_list digits;
+
+		sum = 0;
+		va_start(digits, n);
+		for (i = 0; i < n; i++)
+		{
+			sum = sum + va_arg(digits, unsigned int);
+		}
+		va_end(digits);
+		return (sum);
+	}
+}
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,7 @@
+#ifndef VER_FUNCTION_H
+#define VER_FUNCTION_H
+#include<stdarg.h>
+#include<stdio.h>
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+#endif
