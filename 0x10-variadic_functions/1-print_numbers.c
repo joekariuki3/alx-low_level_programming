abb57de46664ddd9f7d9e3aa2fe1ecc62d615524
joekariuki3 <joekariuki3@gmail.com>
2023-03-23 13:21:56 +0300

    print numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,24 @@
+#include"variadic_functions.h"
+
+/**
+ * print_numbers - prints number arguments passed
+ * @separator: reparater to u sed betwen the numbers
+ * @n: number of arguments passed
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(ap, unsigned int));
+		if (separator != NULL)
+		{
+			printf("%s", separator);
+		}
+	}
+	va_end(ap);
+	printf("\n");
+}
