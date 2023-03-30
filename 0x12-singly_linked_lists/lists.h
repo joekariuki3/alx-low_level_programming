0dd2b36a2e9cdf2ce3f23f91f7e39105fdc9bf1c
joekariuki3 <joekariuki3@gmail.com>
2023-03-30 15:09:08 +0300

    write_handlers

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,9 @@
 #ifndef LINK_LIST_H
 #define LINK_LIST_H
+
+#include<stdio.h>
+#include<stdlib.h>
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -8,15 +12,14 @@
  *
  * Description: singly linked list node structure
  */
-#include<stdlib.h>
-#include<stdio.h>
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 int _putchar(char c);
 size_t print_list(const list_t *h);
+
 #endif
