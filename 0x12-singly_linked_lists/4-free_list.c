ff5c4329a31841238ec0cd9736ca280e47b3e709
joekariuki3 <joekariuki3@gmail.com>
2023-03-30 17:11:48 +0300

    free the list completely

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -11,9 +11,10 @@ void free_list(list_t *head)
 
 	while (head)
 	{
-		tmp = head->next;
-		free(head->str);
-		free(head);
-		head = tmp;
+		tmp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
 	}
+	free(head);
 }
