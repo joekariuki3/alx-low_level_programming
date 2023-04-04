ac3cc4aca3ac09722cc22fb37c8614265b1ef2ce
joekariuki3 <joekariuki3@gmail.com>
2023-04-04 13:42:07 +0300

    delete 1st node of a list

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include"lists.h"
+
+/**
+ * pop_listint - deleted the headnode of a list
+ * @head: pointer to the headnode of the list
+ * Return: headnode integer or headnode data of the removed node
+ */
+int pop_listint(listint_t **head)
+{
+	int rmNodeData;
+	listint_t *temp;
+
+	/* save 1st node data to my variable*/
+	rmNodeData = (*head)->n;
+	/* save 1st node to temp */
+	temp = *head;
+	/* assign second node to head */
+	*head = (*head)->next;
+	/* free the 1st node that we had saved to temp */
+	free(temp);
+	/* Return the saved data from the 1st node one we have just removed*/
+	return (rmNodeData);
+}
