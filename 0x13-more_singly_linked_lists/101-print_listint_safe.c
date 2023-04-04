7f9be5c09eda16149bc48909499cf5463b846b78
joekariuki3 <joekariuki3@gmail.com>
2023-04-04 17:25:05 +0300

    print list safely going throug it once

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+/**
+ * print_listint_safe - print a listint_t linked list including memory addresses
+ * @head: head of linked list
+ * Return: number of nodes in list. If fails, exit with status 98.
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	const listint_t *current;
+	size_t count;
+	const listint_t *hold;
+
+	current = head;
+	if (current == NULL)
+		exit(98);
+
+	count = 0;
+	while (current != NULL)
+	{
+		hold = current;
+		current = current->next;
+		count++;
+		printf("[%p] %d\n", (void *)hold, hold->n);
+
+		if (hold < current)
+		{
+			printf("-> [%p] %d\n", (void *)current, current->n);
+			break;
+		}
+	}
+
+	return (count);
+}
