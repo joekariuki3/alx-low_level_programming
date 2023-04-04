636d12da8cf7174aa0a16b1af62d7b831fd75fd9
joekariuki3 <joekariuki3@gmail.com>
2023-04-04 14:20:23 +0300

    return node n from a list

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,7 +12,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	/* check if list is empty */
 	if (head == NULL)
-		return (NULL)
+		return (NULL);
 	/* loop throug nodes in the list */
 	while (head != NULL)
 	{
