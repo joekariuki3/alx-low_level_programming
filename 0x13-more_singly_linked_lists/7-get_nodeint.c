fd5e54029ef6f2f161dcba2b447732de69caecb3
joekariuki3 <joekariuki3@gmail.com>
2023-04-04 14:12:30 +0300

    return node n from a list

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,26 @@
+#include"lists.h"
+
+/**
+ * get_nodeint_at_index - gets the node a index n
+ * @head: pointer to the 1st node at the list
+ * @index: position of the node to be returned
+ * Return: returns a pointer to node at index n
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int count = 0;
+
+	/* loop throug nodes in the list */
+	while (head != NULL)
+	{
+		count++;
+		head = head->next;
+		if (count == index)
+		{
+			return (head);
+		}
+	}
+	return (NULL);
+
+
+}
