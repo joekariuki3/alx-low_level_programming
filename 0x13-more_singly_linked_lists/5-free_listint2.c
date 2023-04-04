c91ab57556ee3f25a12e33ff7f626e4d64c6e7cc
joekariuki3 <joekariuki3@gmail.com>
2023-04-04 13:09:10 +0300

    free a linked list node by node

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include"lists.h"
+
+/**
+ * free_listint2 - free the list
+ * @head: pointer to the 1st node of the list
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	/* loop throug the list */
+	while (*head != NULL)
+	{
+		/* assign current node to temp */
+		temp = *head;
+		/* move to the next node */
+		*head = (*head)->next;
+		/* free the current node */
+		free(temp);
+	}
+}
