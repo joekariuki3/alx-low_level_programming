d1385e9e8be1dcd3e0ea403cfe2285bee4b98f02
joekariuki3 <joekariuki3@gmail.com>
2023-04-04 16:31:56 +0300

    Add new node at index x in a list

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,5 +1,4 @@
 #include"lists.h"
-
 /**
  * insert_nodeint_at_index - inserts a node at index n
  * @head: pointer to the 1st node of the list
@@ -12,27 +11,20 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i;
 	listint_t *newnode,  *temp;
 
-	/* check if the list is empty if yes we return NULL */
 	if (head == NULL)
 		return (NULL);
 	temp = *head;
-	/* loop throug the list til one node idx */
 	for (i = 1; temp && i < idx; i++)
 	{
 		temp = temp->next;
-		/* check if we reach end of list */
 		if (temp == NULL)
 			return (NULL);
 	}
-	/* asign memory to the newnode*/
 	newnode = malloc(sizeof(listint_t));
 	if (newnode == NULL)
 	{
 		free(newnode);
 		return (NULL);
-	}
-	/* assign n to the new node*/
-	newnode->n = n;
 	if (idx == 0)
 	{
 		*head = newnode;
@@ -40,9 +32,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	else if (temp->next)
 	{
-		/* asign temp next value to new node next value*/
 		newnode->next = temp->next;
-		/* assign temp next value to be address of newnode*/
 		temp->next = newnode;
 	}
 	else
