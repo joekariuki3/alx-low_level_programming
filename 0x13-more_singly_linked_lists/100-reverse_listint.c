6587264ed48c2d0d65b56e0d484ff8bc303de500
joekariuki3 <joekariuki3@gmail.com>
2023-04-04 17:19:00 +0300

    reverse a list

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -9,7 +9,7 @@ listint_t *reverse_listint(listint_t **head)
 	listint_t *hold;
 	listint_t *current;
 
-	if (*head == NULL)
+	if (*head == NULL || head == NULL)
 		return (NULL);
 
 	current = *head;
