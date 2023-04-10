bbd5e90885a188fe84d99feb2e5ca231df6906ce
joekariuki3 <joekariuki3@gmail.com>
2023-04-10 21:02:12 +0300

    Read a text file and print it to the POSIX

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,13 @@
+#ifndef FILE_HANDLING_H
+#define FILE_HANDLING_H
+
+#include<stdio.h>
+#include<stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
