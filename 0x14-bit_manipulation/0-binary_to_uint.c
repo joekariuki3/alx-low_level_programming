562c7d3e6a7337b80c41d3fa85f7539e9b5c565b
joekariuki3 <joekariuki3@gmail.com>
2023-05-04 14:12:22 +0300

    Add 0-binary_to_uint.c
    convetr binary to unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,20 +7,19 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-        unsigned int uint = 0, i = 0;
+	unsigned int uint = 0, i = 0;
 
-        if (b == NULL)
-                return (0);
-        while (b[i] != '\0')
-        {
-                if (b[i] == '0')
-                        uint = uint << 1;
-                else if (b[i] == '1')
-                        uint = (uint << 1) | 1;
-                else
-                        return (0);
-                i++;
-        }
-        return (uint);
+	if (b == NULL)
+		return (0);
+	while (b[i] != '\0')
+	{
+		if (b[i] == '0')
+			uint = uint << 1;
+		else if (b[i] == '1')
+			uint = (uint << 1) | 1;
+		else
+			return (0);
+		i++;
+	}
+	return (uint);
 }
-
