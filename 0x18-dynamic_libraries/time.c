da3d1eed5ab3314a99446537f0b62e1ff56a25ba
joekariuki3 <joekariuki3@gmail.com>
2023-06-19 22:11:10 +0300

    timeExploit

---
+++ 0x18-dynamic_libraries/time.c
@@ -0,0 +1,11 @@
+#include<stdlib.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+time_t time(time_t *__timer)
+{
+	write(1, "9 8 10 24 75 -9\n", 17);
+	write(1, "Congratulations, you win the jackpot!\n", 38);
+	(void)__timer;
+	exit(0);
+}
