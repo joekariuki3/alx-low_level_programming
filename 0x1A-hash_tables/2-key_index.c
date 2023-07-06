dbd8acc2a05134a1f420ec0e18d584b1ad2b6f15
joekariuki3 <joekariuki3@gmail.com>
2023-07-06 13:15:57 +0300

    key -> index

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,15 @@
+#include"hash_tables.h"
+/**
+ * key_index - calculates the position to put data in the array
+ * @key: provided key string
+ * @size: size of the array
+ * Return: the index
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int hash = 0, index = 0;
+
+	hash = hash_djb2(key);
+	index = hash % size;
+	return (index);
+}
