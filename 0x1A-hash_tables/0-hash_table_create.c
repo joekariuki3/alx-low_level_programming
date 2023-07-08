016cad9e9681dbb575e1a9be5f19ab00bc2ed69e
joekariuki3 <joekariuki3@gmail.com>
2023-07-08 13:46:31 +0300

    >>> ht = {}

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,11 +12,16 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	if (size > 0)
 	{
-		table = (hash_table_t *)malloc(sizeof(hash_table_t) * size);
+		table = (hash_table_t *)malloc(sizeof(hash_table_t));
 		if (table == NULL)
 			return (NULL);
+		table->array = (hash_node_t **) malloc(sizeof(hash_node_t *) * size);
+		if (table->array == NULL)
+		{
+			free(table);
+			return (NULL);
+		}
 		table->size = size;
-		table->array = (hash_node_t **) calloc(table->size, sizeof(hash_node_t *));
 		for (i = 0; i < table->size; i++)
 		{
 			table->array[i] = NULL;
