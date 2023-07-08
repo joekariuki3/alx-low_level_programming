48fc4ec05420cbf8206c37e5e5f50bdfea7d4cb6
joekariuki3 <joekariuki3@gmail.com>
2023-07-08 14:38:18 +0300

    fix get element

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,46 +1,30 @@
-#include"hash_tables.h"
+#include "hash_tables.h"
 
 /**
- * hash_table_get - retrieve a value using a key
- * @ht: hash table containing the data
- * @key: string to look for
- * Return: string value or NULL on failure
+ * hash_table_get - Retrieve a value associated with a key
+ * @ht: hash table
+ * @key: key to use to retrieve value
+ * Return: value associated with key or NULL if key not found
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	/* get key index */
-	unsigned long int element_index = 0;
+	hash_node_t *tmp;
+	unsigned long int idx;
 
-	unsigned const char *key_u = NULL;
+	if (ht == NULL || key == NULL)
+		return (NULL);
 
-	hash_node_t *temp = NULL;
+	idx = key_index((const unsigned char *)key, ht->size);
+	tmp = ht->array[idx];
 
-	key_u = (unsigned const char *) key;
-	element_index = key_index(key_u, ht->size);
-	if (element_index > ht->size)
+	if (tmp == NULL)
 		return (NULL);
-	if (ht->array[element_index] == NULL)
+
+	while (strcmp(tmp->key, key) != 0)
+		tmp = tmp->next;
+
+	if (tmp == NULL)
 		return (NULL);
-	if (strcmp(ht->array[element_index]->key, key) == 0)
-	{
-		if (ht->array[element_index]->value != NULL)
-			return (ht->array[element_index]->value);
-		return (NULL);
-	}
-	else
-	{
-		if (ht->array[element_index]->next == NULL)
-			return (NULL);
-		temp = (hash_node_t *)malloc(sizeof(hash_node_t));
-		if (temp == NULL)
-			return (NULL);
-		temp = ht->array[element_index]->next;
-		while (temp->next != NULL)
-		{
-			if (strcmp(temp->key, key) == 0)
-				return (temp->value);
-			temp = temp->next;
-		}
-	}
-	return (NULL);
+
+	return (tmp->value);
 }
