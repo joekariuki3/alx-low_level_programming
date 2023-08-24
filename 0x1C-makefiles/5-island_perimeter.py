7fc62e150a3d3cb705cf1a9afe75128cfbb90288
joekariuki3 <joekariuki3@gmail.com>
2023-08-24 10:31:42 +0300

    Refactor code for island perimeter to check for cells touching each other and subtract those length

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -11,38 +11,28 @@ def island_perimeter(grid):
     1 rep land i.e 1 square of length 1
     0 rep water
     """
-    if grid:
-        landCells = []
-        cellLength = 1
-        perimeter = 0
-        for cells in grid:
-            # check if there is land in row of cells
-            if 1 in cells:
-                for value in cells:
-                    # confirm howmany land cells are
-                    # there in that row and add to a list
-                    if value == 1:
-                        landCells.append(value)
-        # now calculate perimeter if found any land cell
-        if len(landCells) > 0:
-            if len(landCells) == 1:
-                # only one cell
-                perimeter = cellLength * 4
-                return perimeter
+    if not grid:
+        return 0
 
-            first_last_cell_perimeter = 0
-            other_cell_perimeter = 0
-            # use index to see position of the cells to know
-            # wich sedes to add to perimeter
-            for index, cell in enumerate(landCells):
+    rows = len(grid)
+    cols = len(grid[0])
+    squareLength = 1
 
-                # if cell is 1st or last add 3 side of its length
-                if index == 0 or index == len(landCells) - 1:
-                    first_last_cell_perimeter += cellLength * 3
+    perimeter = 0
 
-                # if cell is middle add 2 sides of the length
-                if index > 0 and index < len(landCells) - 1:
-                    other_cell_perimeter += cellLength * 2
+    for row in range(rows):
+        for col in range(cols):
+            # calculate perimeter for the current cell
+            if grid[row][col] == 1:
+                perimeter += (squareLength * 4)
 
-            perimeter = first_last_cell_perimeter + other_cell_perimeter
-            return perimeter
+                # confirm if there is a left cell touching current cell
+                # if yes subtract the two sides touching each other
+                if col > 0 and grid[row][col - 1] == 1:
+                    perimeter -= (squareLength * 2)
+
+                # confirm if there is a right cell touching current cell
+                # if yes subtract the two sides touching each other
+                if row > 0 and grid[row - 1][col] == 1:
+                    perimeter -= (squareLength * 2)
+    return perimeter
