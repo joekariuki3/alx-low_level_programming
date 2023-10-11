b21007ce10bd3b255130db9d08a0e07592008915
joekariuki3 <joekariuki3@gmail.com>
2023-10-11 22:00:09 +0300

    Jump search in a singly linked list

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -8,4 +8,34 @@
  * Return: return pointer to the value or NULL if value is missing
  */
 listint_t *jump_list(listint_t *list, size_t size, int value)
-{}
+{
+	int i = 0, jump = 0;
+	listint_t *current = NULL, *prev = NULL;
+
+	if (list == NULL || size == 0)
+		return (NULL);
+
+	jump = sqrt(size);
+	current = list;
+
+	/*Perform the jump search*/
+	while (current->index < size - 1 && current->n < value)
+	{
+		prev = current;
+		for (i = 0; i < jump && current->next != NULL; i++)
+			current = current->next;
+		printf("Value checked at index [%lu] = [%d]\n", current->index, current->n);
+	}
+
+	/*Perform linear search in the block*/
+	printf("Value found between indexes [%lu] and [%lu]\n",
+			prev->index, current->index);
+	while (prev != NULL && prev->index <= current->index)
+	{
+		printf("Value checked at index [%lu] = [%d]\n", prev->index, prev->n);
+		if (prev->n == value)
+			return (prev);
+		prev = prev->next;
+	}
+	return (NULL);
+}
