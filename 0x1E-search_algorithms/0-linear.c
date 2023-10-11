eb830a163b69eb641194445c90474d680866fd28
joekariuki3 <joekariuki3@gmail.com>
2023-10-11 08:07:12 +0300

    Linear search correction

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -9,46 +9,15 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-	size_t i = 0, j = 0;
-	char string[256] = "Value checked array\n";
+	size_t i = 0;
 
 	if (array == NULL)
 		return (-1);
 	for (i = 0; i < size; i++)
 	{
-		for (j = 0; string[j] != '\n'; j++)
-		{
-			putchar(string[j]);
-		}
-			putchar('[');
-			printInteger((int)i);
-			putchar(']');
-			putchar(' ');
-			putchar('=');
-			putchar(' ');
-			putchar('[');
-			printInteger(array[i]);
-			putchar(']');
-			putchar('\n');
+		printf("Value checked array [%ld] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 			return (i);
 	}
 	return (-1);
-}
-
-/**
- * printInteger - prints number value
- * @num: value to be printed
- * Return: number to be printed
- */
-void printInteger(int num)
-{
-	if (num < 0)
-	{
-		putchar('-');
-		num = -num;
-	}
-	if (num / 10 != 0)
-		printInteger(num / 10);
-	putchar(num % 10 + '0');
 }
