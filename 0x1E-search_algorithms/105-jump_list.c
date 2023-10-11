a7c56a9465fe5be887962aacfa87df80bcf8c6df
joekariuki3 <joekariuki3@gmail.com>
2023-10-11 13:45:19 +0300

    Jump search singly list

---
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -0,0 +1,11 @@
+#include"search_algos.h"
+
+/**
+ * jump_list - search for valur in link list using jump search
+ * @list: pointer to the header of the  list
+ * @size: number of elements in the list
+ * @value: the integet to look for in the list
+ * Return: return pointer to the value or NULL if value is missing
+ */
+listint_t *jump_list(listint_t *list, size_t size, int value)
+{}
